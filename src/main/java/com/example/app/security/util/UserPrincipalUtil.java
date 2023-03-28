b78f980a0d8f489d4baa9434a5b97fbe7a92e9ee
VovaMitsura <vovamitsura@gmail.com>
2023-03-28 10:51:35 +0300

    Add Security util to extract user from sec. context holders. Add processing getting user by email.

---
+++ src/main/java/com/example/app/security/util/UserPrincipalUtil.java
@@ -0,0 +1,14 @@
+package com.example.app.security.util;
+
+import com.example.app.security.SimpleUserPrinciple;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class UserPrincipalUtil {
+
+  public static SimpleUserPrinciple extractUserPrinciple() {
+    return (SimpleUserPrinciple) SecurityContextHolder.getContext()
+        .getAuthentication().getPrincipal();
+  }
+
+
+}
