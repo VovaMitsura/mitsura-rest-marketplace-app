71920c96b37563747282df9b273b795961ba2ea0
VovaMitsura <vovamitsura@gmail.com>
2023-04-07 18:00:34 +0300

    Add endpoint to get current user, write some tests

--- src/main/java/com/example/app/controller/AuthenticationController.java
+++ src/main/java/com/example/app/controller/AuthenticationController.java
@@ -3,32 +3,42 @@ package com.example.app.controller;
 import com.example.app.controller.dto.AuthenticationRequest;
 import com.example.app.controller.dto.AuthenticationResponse;
 import com.example.app.controller.dto.RegisterRequest;
+import com.example.app.security.SimpleUserPrinciple;
+import com.example.app.security.util.UserPrincipalUtil;
 import com.example.app.service.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-  private final AuthenticationService service;
+    private final AuthenticationService authService;
 
-  @PostMapping("/register")
-  public ResponseEntity<AuthenticationResponse> register(
-      @RequestBody RegisterRequest registerRequest) {
-    return new ResponseEntity<>(service.register(registerRequest), HttpStatus.CREATED);
-  }
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+           @Valid @RequestBody RegisterRequest registerRequest) {
+        return new ResponseEntity<>(authService.register(registerRequest), HttpStatus.CREATED);
+    }
 
-  @PostMapping("/authenticate")
-  public ResponseEntity<AuthenticationResponse> register(
-      @RequestBody AuthenticationRequest registerRequest) {
-    return ResponseEntity.ok(service.authenticate(registerRequest));
-  }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @Valid @RequestBody AuthenticationRequest registerRequest) {
+        return ResponseEntity.ok(authService.authenticate(registerRequest));
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("hasAnyAuthority('CUSTOMER', 'SELLER', 'ADMIN')")
+    public ResponseEntity<SimpleUserPrinciple> getMe() {
+
+        SimpleUserPrinciple simpleUserPrinciple = UserPrincipalUtil.extractUserPrinciple();
+
+        return ResponseEntity.ok(simpleUserPrinciple);
+    }
 
 }
