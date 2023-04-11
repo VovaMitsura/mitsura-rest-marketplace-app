d8ff07fbfcdf15ce2a5fc990386e6ccf824b2a16
VovaMitsura <vovamitsura@gmail.com>
2023-04-11 10:35:00 +0300

    Add get product by id endpoint

--- src/main/java/com/example/app/controller/ProductController.java
+++ src/main/java/com/example/app/controller/ProductController.java
@@ -2,37 +2,40 @@ package com.example.app.controller;
 
 import com.example.app.model.Product;
 import com.example.app.service.ProductService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@PreAuthorize("hasAnyAuthority('CUSTOMER', 'SELLER', 'ADMIN')")
 @RequiredArgsConstructor
 public class ProductController {
 
-  private final ProductService productService;
+    private final ProductService productService;
 
-  @GetMapping()
-  @PreAuthorize("hasAnyAuthority('CUSTOMER', 'SELLER', 'ADMIN')")
-  ResponseEntity<List<Product>> getProduct(@RequestParam(required = false, name = "c") String category,
-      @RequestParam(required = false, defaultValue = "0", name = "min") int minCost,
-      @RequestParam(required = false, defaultValue = "1000000", name = "max") int maxCost) {
+    @GetMapping()
+    ResponseEntity<List<Product>> getProduct(@RequestParam(required = false, name = "c") String category,
+                                             @RequestParam(required = false, defaultValue = "0", name = "min") int minCost,
+                                             @RequestParam(required = false, defaultValue = "1000000", name = "max") int maxCost) {
 
-    List<Product> products;
+        List<Product> products;
 
-    if (category == null) {
-      products = productService.findAllByPriceBetween(minCost, maxCost);
-    } else {
-      products = productService.findAllByCategoryAndPriceBetween(category, minCost, maxCost);
+        if (category == null) {
+            products = productService.findAllByPriceBetween(minCost, maxCost);
+        } else {
+            products = productService.findAllByCategoryAndPriceBetween(category, minCost, maxCost);
+        }
+
+        return ResponseEntity.ok(products);
     }
 
-    return ResponseEntity.ok(products);
-  }
+    @GetMapping("/{id}")
+    ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
 
 }
