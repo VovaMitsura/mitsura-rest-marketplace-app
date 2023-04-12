1db591ecf0c21c60974f27c35382c1c07b93464e
VovaMitsura <vovamitsura@gmail.com>
2023-04-12 16:29:02 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,12 @@
 insert into User (fullname, email, role, password)
 values ('John Smith', 'john@mail.com', 'CUSTOMER',
         '$2a$10$8zXeu9h2RKWSh8nqSz4MfuURChXZLbnTzVGHZwPYwn6gOR7PBfiMW'),
-       ('Jack John', 'jack@mail.com', 'ADMIN', '123456'),
+       ('Jack John', 'jack@mail.com', 'ADMIN',
+        '$2a$10$8zXeu9h2RKWSh8nqSz4MfuURChXZLbnTzVGHZwPYwn6gOR7PBfiMW'),
        ('Dan Samuel', 'Samuel@mail.com', 'CUSTOMER', '123456'),
        ('Will Taylor', 'will@mail.com', 'CUSTOMER', '123456'),
-       ('Tanya Smith', 'tanya@mail.com', 'SELLER', '123456');
+       ('Tanya Smith', 'tanya@mail.com', 'SELLER',
+        '$2a$10$8zXeu9h2RKWSh8nqSz4MfuURChXZLbnTzVGHZwPYwn6gOR7PBfiMW');
 
 insert into Bonus (name, date, amount)
 values ('action', now(), 200),
@@ -26,10 +28,10 @@ values ('smartphone', 'smart gadgets for teenagers'),
 for short, is a small, portable personal computer (PC).');
 
 insert into product (name, price, discount_id, category_id, seller_id, quantity)
-values ('Samsung m53', 17500, null, 1, 4, 15),
-       ('Samsung a54', 22000, null, 1, 4, 20),
-       ('Honor h2', 8500, null, 1, 4, 30),
-       ('Asus vivoBook', 22000, null, 2, 4, 10);
+values ('Samsung m53', 17500, null, 1, 5, 15),
+       ('Samsung a54', 22000, null, 1, 5, 20),
+       ('Honor h2', 8500, null, 1, 5, 30),
+       ('Asus vivoBook', 22000, null, 2, 5, 10);
 
 insert into my_order (customer_id, total_amount, date, status)
 values (1, 8500, now(), 'CREATED');
