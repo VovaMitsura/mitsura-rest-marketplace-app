f89fc362a4f7fc813ef03a9e47c6cdc45b10fe16
VovaMitsura <vovamitsura@gmail.com>
2023-05-05 14:30:36 +0300

    Add findByEmailAndStatus methods

--- src/main/java/com/example/app/repository/OrderRepository.java
+++ src/main/java/com/example/app/repository/OrderRepository.java
@@ -1,16 +1,16 @@
 package com.example.app.repository;
 
 import com.example.app.model.Order;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-  List<Order> findAllByCustomerEmail(String userEmail);
-  Optional<Order> findByIdAndCustomerEmail(Long id, String userEmail);
+  Optional<Order> findByIdAndCustomerEmailAndStatus(Long id, String userEmail, Order.Status status);
+  List<Order> findAllByCustomerEmailAndStatus( String userEmail, Order.Status status);
 
 }
