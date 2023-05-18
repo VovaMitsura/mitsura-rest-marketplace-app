b1619a404d54041e244018cfd0444bf285c54cea
VovaMitsura <vovamitsura@gmail.com>
2023-05-18 15:16:32 +0300

    Add grant role method

--- src/main/java/com/example/app/service/UserService.java
+++ src/main/java/com/example/app/service/UserService.java
@@ -6,58 +6,66 @@ import com.example.app.exception.NotFoundException;
 import com.example.app.model.User;
 import com.example.app.model.User.Role;
 import com.example.app.repository.UserRepository;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-  private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-  @Autowired
-  public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
-
-  public List<User> getUsersByRole(Role role) {
-    List<User> users = userRepository.getUsersByRole(role);
-
-    if (users.isEmpty()) {
-      throw new NotFoundException(
-          ApplicationExceptionHandler.USER_NOT_FOUND,
-          String.format("No exists %ss in system", role));
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    return users;
-  }
+    public List<User> getUsersByRole(Role role) {
+        List<User> users = userRepository.getUsersByRole(role);
 
-  public User getUserByIdAndRole(Long id, Role role) {
-    Optional<User> user = userRepository.findByIdAndRole(id, role);
+        if (users.isEmpty()) {
+            throw new NotFoundException(
+                    ApplicationExceptionHandler.USER_NOT_FOUND,
+                    String.format("No exists %ss in system", role));
+        }
 
-    return user.orElseThrow(
-        () -> new NotFoundException(
-            ApplicationExceptionHandler.USER_NOT_FOUND,
-            String.format("No exists %s with id: %d in system", role, id)));
-  }
+        return users;
+    }
 
-  public User getUserByEmail(String email) {
-    Optional<User> user = userRepository.findByEmail(email);
+    public User getUserByIdAndRole(Long id, Role role) {
+        Optional<User> user = userRepository.findByIdAndRole(id, role);
 
-    return user.orElseThrow(() -> new NotFoundException(ApplicationExceptionHandler.USER_NOT_FOUND,
-        String.format("User with email [%s] not found", email)));
-  }
+        return user.orElseThrow(
+                () -> new NotFoundException(
+                        ApplicationExceptionHandler.USER_NOT_FOUND,
+                        String.format("No exists %s with id: %d in system", role, id)));
+    }
 
-  public User updateUser(User update){
-    User currentUser = getUserByEmail(update.getEmail());
+    public User getUserByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
 
-    currentUser.setOrders(update.getOrders());
-    currentUser.setTotalBonusAmount(update.getTotalBonusAmount());
-    currentUser.setOrders(update.getOrders());
-    currentUser.setProducts(update.getProducts());
+        return user.orElseThrow(() -> new NotFoundException(ApplicationExceptionHandler.USER_NOT_FOUND,
+                String.format("User with email [%s] not found", email)));
+    }
 
-    return userRepository.save(currentUser);
-  }
+    public User updateUser(User update) {
+        User currentUser = getUserByEmail(update.getEmail());
+
+        currentUser.setOrders(update.getOrders());
+        currentUser.setTotalBonusAmount(update.getTotalBonusAmount());
+        currentUser.setOrders(update.getOrders());
+        currentUser.setProducts(update.getProducts());
+
+        return userRepository.save(currentUser);
+    }
+
+    public User grantUserRole(String userEmail, Role role) {
+        User user = getUserByEmail(userEmail);
+        user.setRole(role);
+        return userRepository.save(user);
+    }
 
 }
