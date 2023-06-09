3904d0707368a55181d32f6d5ed5cf010207cec9
VovaMitsura <vovamitsura@gmail.com>
2023-06-09 16:04:33 +0300

    Refactor: add noArgs annotation for Jackson

--- src/main/java/com/example/app/controller/dto/ActivationDTO.java
+++ src/main/java/com/example/app/controller/dto/ActivationDTO.java
@@ -2,9 +2,11 @@ package com.example.app.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ActivationDTO {
 
     private String message;
